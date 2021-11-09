8c628fb1d9c07b0bcc9d46c7ed9e393bf0efd06f
onowakow <nowakowskiowen08@gmail.com>
2021-11-09 13:36:28 -0600

    configured nodemon

--- index.js
+++ index.js
@@ -1,4 +1,41 @@
 // As with program wrote in part3-notes, this line is akin to import express from 'express'
-const express = require('express')
-// Call express function and load (I don't believe 'load' really captures what happens here, but IDK what exactly happens) into app.
-const app = express()
+const express = require("express");
+// express is a function, called here to create an app. The app is stored in app.
+const app = express();
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+// request is information of http request. Response is how request is responded to 
+app.get('/', (request, response) => {
+  response.send('<h1>Index</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(notes)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`)
+})
