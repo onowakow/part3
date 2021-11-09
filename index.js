dfea04decb25a79d4b00734535d0606f1c987dda
onowakow <nowakowskiowen08@gmail.com>
2021-11-09 13:31:01 -0600

    Configured express. setting up index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+// As with program wrote in part3-notes, this line is akin to import express from 'express'
+const express = require('express')
+// Call express function and load (I don't believe 'load' really captures what happens here, but IDK what exactly happens) into app.
+const app = express()
